ced8110d0942cdc9d4daed589f3db07d9623fe1d
SOFIA-PC\Sofia <sofia.belchiorinho@b-i.com>
2017-09-13 11:49:26 +0100

    finish udacimeals

---
+++ meal-planner/src/actions/index.js
@@ -0,0 +1,23 @@
+//actions
+
+export const ADD_RECIPE = 'ADD_RECIPE'
+export const REMOVE_FROM_CALENDAR = 'REMOVE_FROM_CALENDAR'
+
+//action creators
+
+export function addRecipe({day, recipe, meal}){
+	return {
+		type: ADD_RECIPE,
+		recipe,
+		day,
+		meal
+	}
+}
+
+export function removeFromCalendar({day, meal}){
+	return {
+		type: REMOVE_FROM_CALENDAR,
+		day, 
+		meal
+	}
+}
